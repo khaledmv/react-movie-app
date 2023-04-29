e7d944dab6ab07321e77e71fd35ff57c5c35a269
khaledmv <khaledhossionmv@gmail.com>
2023-04-29 12:38:39 +0600

    fetch movie api

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,7 +1,27 @@
 
+import { useEffect, useState } from "react"
+import { Card } from "../components"
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies(){
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=cbb45f2e1f3aacd9f39cf81baff7d5d4&language=en-US&page=1");
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
   return (
-    <main>MovieList</main>
+    <main >
+      <section className="w-full mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+         { movies.map((movie) => (
+          <Card key={movie.id} movie={movie}  />
+         ))}
+        </div>
+      </section>
+    </main>
   )
 }
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -4,16 +4,16 @@ import { MovieDetail, MovieList,Search,PageNotFound } from "../pages";
 
  export const AllRoutes = () => {
      return(
-        <>
+        <div className="dark:bg-slate-800">
             <Routes>
                 <Route path="" element={<MovieList/>} />
-                <Route path="/movies/:id" element={<MovieDetail/>} />
+                <Route path="/movie/:id" element={<MovieDetail/>} />
                 <Route path="/movies/popular" element={<MovieList/>} />
                 <Route path="/movies/top" element={<MovieList/>} />
                 <Route path="/movies/upcoming" element={<MovieList/>} />
                 <Route path="/movies/search" element={<Search/>} />
                 <Route path="*" element={<PageNotFound/>} />
             </Routes>
-        </>
+        </div>
      )
  }
